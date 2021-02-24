8125dd73b574d86cccc1324f83eca91855a463bf
maureenlholland <maureen.holland11@gmail.com>
2021-02-24 17:34:11 +0000

    add image and partials, config prettier

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,9 @@
 module.exports = (config) => {
+  config.addPassthroughCopy("./src/images/");
   return {
+    markdownTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
     dir: {
       input: "src",
       output: "dist",
