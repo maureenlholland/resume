36fb6f81e051a0f028e905e2456f19d6a43db7d6
maureenlholland <maureen.holland11@gmail.com>
2021-03-12 15:19:26 +0000

    remove placeholder image, clean, add image processing and base style

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,27 @@
+const Image = require("@11ty/eleventy-img");
+
+// 11ty plugin to generate multiple image sizes and formats: https://www.11ty.dev/docs/plugins/image/
+async function imageShortcode(src, alt, sizes) {
+  let metadata = await Image(src, {
+    widths: [300, 600],
+    formats: ["webp", "jpeg"],
+    // this is how I want the src attribute to be prefixed
+    urlPath: "/images/",
+    // this is where I want the processed images to live
+    outputDir: "./dist/images/",
+  });
+
+  let imageAttributes = {
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async",
+    class: "u-photo",
+  };
+
+  return Image.generateHTML(metadata, imageAttributes);
+}
+
 module.exports = (config) => {
   // Tell 11ty to use the .eleventyignore and ignore our .gitignore file
   config.setUseGitIgnore(false);
@@ -5,6 +29,8 @@ module.exports = (config) => {
   config.addWatchTarget("./src/styles/");
   // Don't create a page for images/index.html
   config.addPassthroughCopy("./src/images/");
+  // Use shortcode to process images with above plugin
+  config.addNunjucksAsyncShortcode("image", imageShortcode);
   // Collections to grab in templates
   config.addCollection("education", (collection) => {
     // worthwhile to add a helper to fill in org details on entries instead of creating another org collection for reference in templates?
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,9 @@
 const { parallel, watch } = require("gulp");
 const sass = require("./gulp-tasks/sass.js");
-const browserSync = require("browser-sync").create();
 
 // Set directory to watch and assign task
 const watcher = () => {
-  watch("./src/styles/**/*.scss", sass).on("change", browserSync.reload);
+  watch("./src/styles/**/*.scss", sass);
 };
 
 // default `gulp` command behaviour is to run each task in parrallel
