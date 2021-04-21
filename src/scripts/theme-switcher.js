5bc5dd7321c6dc4e91e1a19e53b87d639e2a9812
maureenlholland <maureen.holland11@gmail.com>
2021-04-21 16:28:44 +0100

    add concat gulp task, add esc and click outside auto-close

--- src/scripts/theme-switcher.js
+++ src/scripts/theme-switcher.js
@@ -6,6 +6,7 @@ class ThemeSwitcher {
     this.hasLocalStorage = typeof Storage !== "undefined";
     this.svg = document.getElementById("photo__themes");
     this.toggle = document.getElementById("theme-switcher__display");
+    this.themeMenu = document.getElementById("theme-switcher__options");
     this.themeOptions = document.querySelectorAll(".theme-switcher__option");
 
     this.init();
@@ -34,28 +35,44 @@ class ThemeSwitcher {
         option.addEventListener("click", () => this.setTheme(id));
       }
     });
-    // Options for the observer (which mutations to observe)
-    const config = { attributes: true, childList: true, subtree: true };
 
-    // Callback function to execute when mutations are observed
-    const callback = function (mutationsList, observer) {
-      // Use traditional 'for loops' for IE 11
-      for (const mutation of mutationsList) {
-        if (mutation.type === "attributes") {
-          if (mutation.target.ariaExpanded === "true") {
-            document.documentElement.setAttribute("data-switcher-open", true);
-          } else {
-            document.documentElement.setAttribute("data-switcher-open", false);
-          }
+    // need the exact same function object to reference to remove this event listener, using bind instead of arrow function here
+    this.handleThemeMenu = this.handleThemeMenu.bind(this);
+    this.forceCloseThemeMenu = this.forceCloseThemeMenu.bind(this);
+    // observe aria-expanded attribute on toggle button
+    new MutationObserver(this.handleThemeMenu).observe(this.toggle, {
+      attributes: true,
+      attributeFilter: ["aria-expanded"],
+    });
+  }
+
+  handleThemeMenu(mutationsList) {
+    // Use traditional 'for loops' for IE 11
+    for (const mutation of mutationsList) {
+      if (mutation.type === "attributes") {
+        if (mutation.target.ariaExpanded === "true") {
+          /*
+            When theme menu is expanded
+            - blur background
+            - tab trap inside theme menu
+            - listen for ESC or outside theme menu event
+            */
+          document.documentElement.setAttribute("data-switcher-open", true);
+          document.addEventListener("keydown", this.forceCloseThemeMenu);
+          document.addEventListener("click", this.forceCloseThemeMenu);
+        } else {
+          /*
+            When theme menu is closed
+            - do not blur background
+            - remove tab trap
+            - remove ESC or outside theme menu event listeners
+          */
+          document.documentElement.setAttribute("data-switcher-open", false);
+          document.removeEventListener("keydown", this.forceCloseThemeMenu);
+          document.removeEventListener("click", this.forceCloseThemeMenu);
         }
       }
-    };
-
-    // Create an observer instance linked to the callback function
-    let observer = new MutationObserver(callback);
-
-    // Start observing the target node for configured mutations
-    observer.observe(this.toggle, config);
+    }
   }
 
   getSystemPreference() {
@@ -72,10 +89,31 @@ class ThemeSwitcher {
     return false;
   }
 
-  toggleOptionDisplay() {
-    const currentState =
-      this.toggle.getAttribute("aria-expanded") === "false" ? false : true;
-    this.toggle.setAttribute("aria-expanded", String(!currentState));
+  toggleOptionDisplay(options = {}) {
+    if (options.forceClose) {
+      this.toggle.setAttribute("aria-expanded", false);
+      // set focus on toggle button after menu closes
+      this.toggle.focus();
+    } else {
+      const currentState =
+        this.toggle.getAttribute("aria-expanded") === "false" ? false : true;
+      this.toggle.setAttribute("aria-expanded", String(!currentState));
+    }
+  }
+
+  tabTrapThemeMenu() {}
+
+  forceCloseThemeMenu(e) {
+    if (e.type === "keydown" && e.key === "Escape") {
+      return this.toggleOptionDisplay({ forceClose: true });
+    }
+    if (
+      e.type === "click" &&
+      !e.path.includes(this.themeMenu) &&
+      !e.path.includes(this.toggle)
+    ) {
+      return this.toggleOptionDisplay({ forceClose: true });
+    }
   }
 
   setTheme(id) {
